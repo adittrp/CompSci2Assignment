af9cb9b5593d9000f0f55e813f609abbfd2e8b08
adit.tripathi <adittrp08@gmail.com>
2024-04-11 14:32:09 -0500

    Main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pygame.init()
 window = pygame.display.set_mode((1180, 720))
 window.fill((128, 60, 60))
 
-chessboard = pygame.image.load("chess board.png").convert()
+chessboard = pygame.image.load("Files/chess board.png").convert()
 window.blit(chessboard, (0, 0))
 
 chessboard_squares = []
@@ -15,7 +15,7 @@ for l in range(8):
     for n in range(8):
         chessboard_column = [(40 + (n * 80), 40 + (l * 80)), chessboard_letters[n] + str(l + 1)]
 
-        king_piece = pygame.image.load("King.png").convert_alpha()
+        king_piece = pygame.image.load("Files/King.png").convert_alpha()
         window.blit(king_piece, (chessboard_column[0][0], chessboard_column[0][1]))
 
         chessboard_row.append(chessboard_column)
